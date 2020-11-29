e0e0128a348452169b32e27d5743665e48945719
Rick Brown <ricky.brown.00@gmail.com>
2020-11-29 17:12:25 +0000

    Initial Commit

---
+++ weSplit/weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitApp.swift
+//  weSplit
+//
+//  Created by Rick Brown on 29/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
